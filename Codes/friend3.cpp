df8f9e85b0aab6f5abd82ecf8823618470d461cf
Abhishek Atole <85939904+Abhishek-Atole@users.noreply.github.com>
2025-05-18 14:03:22 +0000

    friend in C++

--- Codes/friend3.cpp
+++ Codes/friend3.cpp
@@ -3,44 +3,43 @@ using namespace std;
 
 class Hello
 {
-public:
-    int a;
-    void Display();
+    public:
+        void Display();   //function is defined outside the class
 };
 class Demo
 {
-public:
-    int a;
+    public:
+        int a;
 
-private:
-    int b;
+    private:
+        int b;
 
-protected:
-    int c;
+    protected:
+        int c;
 
-public:
-    Demo()
-    {
-        a = 10;
-        b = 20;
-        c = 30;
-    }
-    friend void Hello ::Display(); // Declare Display as a friend function
-    // By write friend void Display(); we are giving access to all the members of class Demo to function Display
-    // alll the members which are are non publublic are accessible to Display
+    public:
+        Demo()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+        }
+        friend void Hello ::Display(); // Declare Display as a friend function
+        // By write friend void Display(); we are giving access to all the members of class Demo to function Display
+        // alll the members which are are non publublic are accessible to Display
 };
-
-void Hello :: Display()
+//Return_type_value class_Name :: function_name
+void Hello :: Display() // it just like a member function of class Hello
 {
     Demo obj;                        // object of class Demo is created
     cout << "a = " << obj.a << endl; // Accessible because it's public
-    cout << "b = " << obj.b << endl; // Not accessible because it's private
-    cout << "c = " << obj.c << endl; // Not accessible because it's protected and void display() is not a friend of Demo
-    // and also it is not a derived class of Demo
+    cout << "b = " << obj.b << endl; // Accessible because it's private and Display() is a friend of Demo
+    cout << "c = " << obj.c << endl; // Accessible because it's protected and Display() is a friend of Demo
 }
+
 int main()
 {
-    Hello obj1;
-    obj1.Display();
+    Hello hobj;
+    hobj.Display();
     return 0;
-}
+} 
